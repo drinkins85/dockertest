02b2ff651ee3e079564b53fd695818eeee6a2cb2
Andre <drinkins85@mail.ru>
2018-03-25 12:36:10 +0300

    1

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:carbon
+
+WORKDIR .
+
+RUN apt-get update
+
+RUN apt-get install -y git
+RUN git clone https://github.com/drinkins85/node-git.git newrepo
+
+COPY package*.json ./
+
+RUN npm install
+RUN npm run build
+
+# If you are building your code for production
+# RUN npm install --only=production
+
+# Bundle app source
+COPY . .
+
+EXPOSE ${PORT}
+
+RUN npm install --quient
+RUN npm run build
+
+CMD [ "npm", "start" ]
---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const childProcess = require('child_process');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.send('Hello, Docker');
+});
+
+app.get('/git', (req, res) => {
+  const git = childProcess.spawn('git', ['status']);
+  // const git = childProcess.spawn('ls');
+  const out = [];
+  git.stdout.on('data', (data) => {
+    out.push(data);
+  });
+  git.stderr.on('data', (data) => {
+    out.push(data);
+  });
+  git.on('close', (code) => {
+    console.log(out.toString());
+    res.send(`${out.toString()}`);
+  });
+});
+
+app.get('/gitcwd', (req, res) => {
+  const cwd = req.query.cwd;
+  const git = childProcess.spawn('git', ['status'], { cwd });
+  // const git = childProcess.spawn('ls');
+  const out = [];
+  git.stdout.on('data', (data) => {
+    out.push(data);
+  });
+  git.stderr.on('data', (data) => {
+    out.push(data);
+  });
+  git.on('close', (code) => {
+    console.log(out.toString());
+    res.send(`${out.toString()}`);
+  });
+});
+
+app.get('/gitclone', (req, res) => {
+  const git = childProcess.spawn('git', ['clone', 'https://github.com/drinkins85/node-git.git', 'n--rep']);
+  // const git = childProcess.spawn('ls');
+  const out = [];
+  git.stdout.on('data', (data) => {
+    out.push(data);
+  });
+  git.stderr.on('data', (data) => {
+    out.push(data);
+  });
+  git.on('close', (code) => {
+    console.log(out.toString());
+    res.send(`res: ${out.toString()}`);
+  });
+});
+
+app.get('/ls', (req, res) => {
+  const git = childProcess.spawn('ls');
+  // const git = childProcess.spawn('ls');
+  const out = [];
+  git.stdout.on('data', (data) => {
+    out.push(data);
+  });
+  git.stderr.on('data', (data) => {
+    out.push(data);
+  });
+  git.on('close', (code) => {
+    console.log(out.toString());
+    res.send(`${out.toString()}`);
+  });
+});
+
+
+app.listen(port, host);
+
+console.log(`App listen on port ${port}`);
