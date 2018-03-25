cc220021472192369a5ca7716f2b0511e1f7829a
Andre <drinkins85@mail.ru>
2018-03-25 22:18:11 +0300

    Prod

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const host = '0.0.0.0';
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-  res.send('Hello, Docker Woo');
+  res.send('Hello, Docker, it is Production');
 });
 
 app.get('/git', (req, res) => {
