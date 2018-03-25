d0520d25902b698f31de3ad8fe3febbc5c46c137
Andre <drinkins85@mail.ru>
2018-03-25 14:35:06 +0300

    10

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const host = '0.0.0.0';
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-  res.send('Hello, Docker');
+  res.send('Hello, Docker 11');
 });
 
 app.get('/git', (req, res) => {
